ac5f841c6f768a3c17a1aca16c856a780adb5d50
eghc <eringraceharris@gmail.com>
2018-11-22 21:58:15 -0800

    checkpoint bloc jams playerbar range complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -16,6 +16,8 @@ class Album extends Component{
     this.state = {
       album: album,
       currentSong: album.songs[0],
+      currentTime: 0,
+      duration: album.songs[0].duration,
       isPlaying: false,
       display: display
     };
@@ -106,6 +108,30 @@ class Album extends Component{
     this.play();
   }
 
+  handleTimeChange(e){
+    const newTime = this.audioElement.duration * e.target.value;
+    this.audioElement.currentTime = newTime;
+    this.setState({currentTime: newTime});
+  }
+
+  componentDidMount(){
+    this.eventListeners = {
+      timeupdate: e =>{
+        this.setState({currentTime: this.audioElement.currentTime});
+      },
+      durationchange: e => {
+         this.setState({duration: this.audioElement.duration});
+      }
+    };
+    this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+    this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
+  }
+
+  componentWillUnmount(){
+    this.audioElement.src = null;
+    this.audioElement.removeEventListener('timeupdate', this.eventListeners.timeupdate);
+    this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
+  }
 
   render(){
     return(
@@ -140,9 +166,12 @@ class Album extends Component{
         </table>
         <PlayerBar isPlaying={this.state.isPlaying}
           currentSong = {this.state.currentSong}
+          currentTime = {this.audioElement.currentTime}
+          duration = {this.audioElement.duration}
           handleSongClick={() => this.handleSongClick(this.state.currentSong, this.state.album.songs.findIndex(song => this.state.currentSong === song))}
           handlePrevClick={() => this.handlePrevClick()}
           handleNextClick={() => this.handleNextClick()}
+          handleTimeChange={(e) => this.handleTimeChange(e)}
         />
       </section>
     );
