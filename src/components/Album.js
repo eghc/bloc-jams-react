3c900a37b1e3791b3ba33a04b11d4b0fdf89b341
eghc <eringraceharris@gmail.com>
2018-11-22 21:43:41 -0800

    assignment bloc jams playerbar buttons complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -95,6 +95,17 @@ class Album extends Component{
     this.play();
   }
 
+  handleNextClick(){
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    if(currentIndex === this.state.album.songs.length-1){
+      return;
+    }
+    const newIndex = Math.max(0, currentIndex +1);
+    const newSong = this.state.album.songs[newIndex];
+    this.handleSongClick(newSong, newIndex);
+    this.play();
+  }
+
 
   render(){
     return(
@@ -131,6 +142,7 @@ class Album extends Component{
           currentSong = {this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong, this.state.album.songs.findIndex(song => this.state.currentSong === song))}
           handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
         />
       </section>
     );
